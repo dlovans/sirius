af684923455ac8a7f53fd7c39dd4f2a39fe0c82e
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-12 15:22:20 +0200

    Define verse view

---
+++ src/lib/ui/VerseView/VerseView.svelte
@@ -0,0 +1,12 @@
+<script>
+	import VerseNumberView from '$lib/ui/VerseView/NumberView.svelte';
+	import TextVerseView from '$lib/ui/VerseView/TextView.svelte';
+
+	export let verseNumber;
+	export let verseText;
+</script>
+
+<div class="flex flex-row flex-nowrap w-auto bg-sky-950">
+	<VerseNumberView verseNumber={verseNumber} />
+	<TextVerseView text={verseText} />
+</div>
