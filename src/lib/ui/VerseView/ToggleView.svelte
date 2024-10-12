e9347beb79b19c028baab54f67f2940ff4eebde4
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-12 22:27:30 +0200

    Define verse toggler

---
+++ src/lib/ui/VerseView/ToggleView.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	export let displayPlusSign = true;
+	export let id;
+
+	let iconRef;
+
+	const dispatch = createEventDispatcher()
+
+	function handleClick() {
+		if (displayPlusSign) {
+			displayPlusSign = false;
+
+			dispatch('addToBookmarked', id)
+		} else {
+			displayPlusSign = true;
+			dispatch('removeFromBookmarked', id)
+		}
+	}
+</script>
+
+<button>
+	<img bind:this={iconRef} on:click={handleClick} src="" alt="Add verse.">
+</button>
