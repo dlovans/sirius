e8a65d8712697132fd953a969a3fdadc521b060b
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-12 22:27:42 +0200

    Compose toggler view

--- src/lib/ui/VerseView/VerseView.svelte
+++ src/lib/ui/VerseView/VerseView.svelte
@@ -1,12 +1,15 @@
 <script>
 	import VerseNumberView from '$lib/ui/VerseView/VerseNumberView.svelte';
 	import TextVerseView from '$lib/ui/VerseView/TextView.svelte';
+	import ToggleView from '$lib/ui/VerseView/ToggleView.svelte';
 
 	export let verseNumber;
 	export let verseText;
+	export let id;
 </script>
 
 <div class="flex flex-row flex-nowrap w-auto bg-sky-950">
 	<VerseNumberView verseNumber={verseNumber} />
 	<TextVerseView text={verseText} />
+	<ToggleView id={id} />
 </div>
