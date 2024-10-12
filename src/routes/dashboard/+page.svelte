6c44cae40adb83dc0c3a554ec2add72506fb4053
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-13 01:40:54 +0200

    Define dashboard route

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import BoardView from '$lib/ui/BoardView/BoardView.svelte';
+	import BookView from '$lib/ui/BookView/BookView.svelte';
+	import MenuView from '$lib/ui/MenuView/MenuView.svelte';
+	import ResearchView from '$lib/ui/ResearchView/ResearchView.svelte';
+	import VerseView from '$lib/ui/VerseView/VerseView.svelte';
+</script>
+
+<div>
+	<MenuView />
+	<ResearchView />
+	<VerseView />
+	<BookView />
+</div>
