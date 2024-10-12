f6559f236933a3b8e058b7a8749120365122a057
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-12 22:16:29 +0200

    Define book view

---
+++ src/lib/ui/BookView/BookView.svelte
@@ -0,0 +1,11 @@
+<script>
+	import VerseView from '$lib/ui/VerseView/VerseView.svelte'
+
+	export let verses;
+</script>
+
+<div class="flex flex-col w-full">
+	{#each verses as verse}
+		<VerseView verseNumber={verse.number} verseText={verse.text} />
+	{/each}
+</div>
