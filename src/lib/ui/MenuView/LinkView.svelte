29bebb627ebe9c2c7e9aad01da4f0db87ae1713b
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-17 01:48:54 +0200

    Define link view

---
+++ src/lib/ui/MenuView/LinkView.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let uri;
+	export let linkText;
+</script>
+
+<a href="dashboard/{uri}"
+	 class="text-zinc-200 bg-sky-900 py-1.5 px-2 flex justify-center items-center hover:bg-sky-600"
+>{linkText}</a>
