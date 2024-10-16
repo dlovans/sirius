a13ad5be5b68909d18bde7376076dad6f7e6ded2
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-17 01:49:09 +0200

    Add link views to side menu

--- src/lib/ui/MenuView/MenuView.svelte
+++ src/lib/ui/MenuView/MenuView.svelte
@@ -1,7 +1,9 @@
 <script>
 	import BoardView from '$lib/ui/BoardView/BoardView.svelte';
+	import LinkView from '$lib/ui/LinkView/LinkView.svelte';
 
 	export let boards;
+	export let adminAccess;
 
 	function handleClick() {
 		console.log("Create board clicked")
@@ -9,6 +11,12 @@
 </script>
 
 <nav class="flex absolute translate-x-[-150%] transition-transform duration-200 ease-in-out w-full overflow-x-hidden">
+	<div>
+		<LinkView uri="/quran" linkText="Qur'an" />
+		{#if adminAccess}
+			<LinkView uri="/dashboard/admin" linkText="Administrative" />
+		{/if}
+	</div>
 	<div>
 		<button on:click={handleClick}
 			class="justify-center items-center w-full h-full px-2.5 py-1.5 bg-sky-950 hover:bg-cyan-950">New Board</button>
