5b086f67396d9669b9d34ba1f60d3044fafdfde6
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-18 23:12:00 +0200

    Add search input tag

---
+++ src/lib/ui/BookView/BookSearchView.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	function handleInput(event) {
+
+	}
+</script>
+
+<input class="bg-sky-200 border-r-8" type="search" placeholder="Search..." />
