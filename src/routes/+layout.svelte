2378d06554190b89142828aac32ac966b92f6df6
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-22 00:55:13 +0200

    Set stores for users and admins

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
 	import "../app.css";
+
+	import { user, isAdmin } from '$lib/stores/auth.js';
+	import { onMount } from 'svelte';
+
+	export let data;
+
+	onMount(() => {
+		user.set(data.isLoggedIn)
+		isAdmin.set(data.isAdmin);
+	})
 </script>
 
 <slot />
