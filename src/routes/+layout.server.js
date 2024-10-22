f5b9885ccf792873e168f49fa79c4ab494610f8d
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-22 22:58:06 +0200

    Get userID from cookies

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,10 @@
 export async function load({ cookies }) {
 	const isLoggedIn = cookies.get('isLoggedIn') || false
+	const userID = cookies.get('userID') || null
 	const isAdmin = cookies.get('isAdmin') || false
 
 	return {
+		userID,
 		isLoggedIn,
 		isAdmin
 	}
