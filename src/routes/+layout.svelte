619314e80f82cfb8efae93b273eaf9d44b06ffa1
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-22 22:58:54 +0200

    Set userID store in main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <script>
 	import "../app.css";
 
-	import { user, isAdmin } from '$lib/stores/auth.js';
+	import { isLoggedIn, isAdmin, userID } from '$lib/stores/auth.js';
 	import { onMount } from 'svelte';
 
 	export let data;
 
 	onMount(() => {
-		user.set(data.isLoggedIn)
-		isAdmin.set(data.isAdmin);
+		isLoggedIn.set(data.isLoggedIn)
+		isAdmin.set(data.isAdmin)
+		userID.set(data.userID)
 	})
 </script>
 
