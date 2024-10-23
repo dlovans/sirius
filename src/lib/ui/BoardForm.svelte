87afccdd50679265f483ee046a14a1d6ebe260f1
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-23 23:16:39 +0200

    Remove hidden input, retrieve data from cookies

--- src/lib/ui/BoardForm.svelte
+++ src/lib/ui/BoardForm.svelte
@@ -49,7 +49,6 @@
 <form action="?/create-board" method="POST" on:submit={submitForm} class="px-3 py-2 rounded-md border-0 bg-gray-800 z-50 flex flex-col justify-center items-center gap-2 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 	<h4>Create Board</h4>
 	<input bind:this={inputRef} on:input={confirmValidity} type="text" name="title" class="border-2 border-gray-500 rounded-md outline-0 px-1 py-1">
-	<input type="hidden" name="userID" value={userID} />
 	<p bind:this={warningTextRef} class="opacity-0 transition-opacity duration-300 bg-orange-400 px-3">Enter a title!</p>
 	<div class="flex justify-between w-full">
 		<button type="submit" class="bg-emerald-300 py-1 px-4">Create</button>
