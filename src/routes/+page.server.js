c1d48dc5e836f31e54a7b8b6f00130dac826bf8a
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-23 23:16:17 +0200

    Define createBoard form action

---
+++ src/routes/+page.server.js
@@ -0,0 +1,36 @@
+import { redirect, fail, error } from '@sveltejs/kit';
+import { createBoard } from '$lib/db/board.js'
+import { userExists } from '$lib/db/user.js'
+
+export const actions = {
+	createBoard: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const boardTitle = data.get('title');
+		const userID = cookies.get('userID');
+
+		if (!boardTitle) {
+			return fail(400, {message: 'Board title is required!'})
+		}
+
+		if (!userID) {
+			throw error(401, 'Unauthorized! Login or sign up to create a board.')
+		}
+
+		const user = await userExists(userID);
+
+		if (!user) {
+			throw error(401, 'Unauthorized user. Login or sign up to create a board.')
+		}
+
+		const result = await createBoard(userID, boardTitle)
+
+		if (result) {
+			if (!result.boardID) {
+				throw error(400, "Couldn't retrieve board details.")
+			}
+			return redirect(303, `/${result.boardID}`)
+		} else {
+			throw error(500, 'Failed to create board. Please try again.')
+		}
+	}
+}
