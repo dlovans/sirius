a5489e386c9d78a21dd49081f2af79ce48d3b07d
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-24 21:17:59 +0200

    Define createBoard function querying database

--- src/lib/db/board.js
+++ src/lib/db/board.js
@@ -1,3 +1,35 @@
-export async function createBoard(boardTitle) {
-	return false
+import { supabase } from '$lib/db/supabase.js';
+import { userExists } from '$lib/db/user.js';
+
+export async function createBoard(userID, boardTitle) {
+	const findUser = await userExists(userID);
+	if (!findUser.success) {
+		return {
+			success: false,
+			message: 'Failed to find user',
+			boardID: null,
+		}
+	}
+
+	let { data: boards, error } = await supabase
+		.from('boards')
+		.insert([
+			{ owner: findUser.userRowId, title: boardTitle }
+		])
+		.select()
+
+	if (error) {
+		console.error('Error creating board: ', error);
+		return {
+			success: false,
+			message: 'An error occurred while creating board',
+			boardID: null,
+		}
+	}
+
+	return {
+		success: true,
+		message: 'Successfully created board',
+		boardID: boards[0]['id']
+	}
 }
