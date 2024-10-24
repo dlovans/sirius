ee1e046b9da576646086e2868d5e4d7e2d191c63
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-24 21:17:45 +0200

    Refactor userExists function

--- src/lib/db/user.js
+++ src/lib/db/user.js
@@ -8,15 +8,30 @@ import { supabase } from '$lib/db/supabase.js';
 export async function userExists(userID) {
 	let { data: users, error } = await supabase
 		.from('users')
-		.select('auth_user_id')
+		.select('id')
 		.eq('auth_user_id', userID)
 		.limit(1)
 
 	if (error) {
 		console.error('Error checking if user exists: ', error)
-		return { success: false, message: 'An error occurred while checking user existence.'}
+		return {
+			success: false,
+			message: 'An error occurred while checking user existence.',
+			userRowId: null
+		}
 	}
 
-	const userExists = !!users.length
-	return { success: true, userExists }
+	if (users.length > 0) {
+		return {
+			success: true,
+			message: 'User found',
+			userRowId: users[0]['id']
+		}
+	} else {
+		return {
+			success: false,
+			message: 'User not found',
+			userRowId: null
+		}
+	}
 }
