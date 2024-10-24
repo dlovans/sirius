3b67def8e4cb1a4e3b449d255596a589fa6cc2c4
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-24 21:44:37 +0200

    Refactor form action createBoardAction

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,9 @@
 import { redirect, fail, error } from '@sveltejs/kit';
 import { createBoard } from '$lib/db/board.js'
-import { userExists } from '$lib/db/user.js'
+import { findUser } from '$lib/db/user.js'
 
 export const actions = {
-	createBoard: async ({ cookies, request }) => {
+	createBoardAction: async ({ cookies, request }) => {
 		const data = await request.formData();
 		const boardTitle = data.get('title');
 		const userID = cookies.get('userID');
@@ -11,27 +11,24 @@ export const actions = {
 		if (!boardTitle) {
 			return fail(400, {message: 'Board title is required!'})
 		}
-
 		if (!userID) {
 			throw error(401, 'Unauthorized! Login or sign up to create a board.')
 		}
 
-		const user = await userExists(userID);
+		const user = await findUser(userID);
 
+		if (!user.userRowId) {
+			throw error(401, user.message)
+		}
 		if (!user.success) {
-			throw error(500, 'Something went wrong! Could not create board.')
+			throw error(500, user.message)
 		}
 
-		if (!user.userExists) {
-			throw error(401, 'Unauthorized user. Login or sign up to create a board.')
-		}
-
-		const result = await createBoard(userID, boardTitle)
+		const result = await createBoard(user.id, boardTitle)
 
 		if (!result.success) {
-			throw error(500, 'Something went wrong! Could not create board.')
+			throw error(500, result.message)
 		}
-
 		if (!result.boardID) {
 			return fail(503, { message: 'Successfully created board. Missing board data. Refresh page.'})
 		}
