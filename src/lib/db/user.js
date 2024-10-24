5e0d49a359523b90d8743e9afc07c652268cbb4f
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-24 17:50:39 +0200

    Define userExists function, checking if a user exists

--- src/lib/db/user.js
+++ src/lib/db/user.js
@@ -1,3 +1,22 @@
+import { supabase } from '$lib/db/supabase.js';
+
+/**
+ * Checks if user exists.
+ * @param userID - User ID.
+ * @returns {object} - If database querying was successful.
+ */
 export async function userExists(userID) {
-	return false
+	let { data: users, error } = await supabase
+		.from('users')
+		.select('auth_user_id')
+		.eq('auth_user_id', userID)
+		.limit(1)
+
+	if (error) {
+		console.error('Error checking if user exists: ', error)
+		return { success: false, message: 'An error occurred while checking user existence.'}
+	}
+
+	const userExists = !!users.length
+	return { success: true, userExists }
 }
