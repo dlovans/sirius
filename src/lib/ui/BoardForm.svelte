2f564b25422e16eb132cdc17f368e5950a320cb3
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-26 00:51:50 +0200

    Add clickable overlay for BoardForm.svelte

--- src/lib/ui/BoardForm.svelte
+++ src/lib/ui/BoardForm.svelte
@@ -16,16 +16,16 @@
 
 	async function submitForm(event) {
 		event.preventDefault();
-		console.log($userID)
 		if (!$userID) {
 			goto('/login')
-			console.log("here")
 			return
 		}
+
 		if (!inputRef.value) {
 			warningTextRef.classList.remove('opacity-0')
 			inputRef.classList.remove('border-gray-500')
 			inputRef.classList.add('border-orange-500')
+			inputRef.focus()
 
 			setTimeout(() => {
 				warningTextRef.classList.add('opacity-0')
@@ -52,13 +52,14 @@
 </script>
 
 {#if displayModal}
-<form action="?/createBoardAction" method="POST" on:submit={submitForm} class="px-3 py-2 rounded-md border-0 bg-gray-800 z-50 flex flex-col justify-center items-center gap-2 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-	<h4>Create Board</h4>
-	<input bind:this={inputRef} on:input={confirmValidity} type="text" name="title" class="border-2 border-gray-500 rounded-md outline-0 px-1 py-1">
-	<p bind:this={warningTextRef} class="opacity-0 transition-opacity duration-300 bg-orange-400 px-3">Enter a title!</p>
+<form action="?/createBoardAction" method="POST" on:submit={submitForm} class="px-5 py-3 rounded-md border-0 bg-gray-800 z-50 flex flex-col justify-center items-center gap-2 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+	<h4 class="text-xl font-bold">Board Title</h4>
+	<input bind:this={inputRef} on:input={confirmValidity} type="text" name="title" class="border-2 border-gray-500 rounded-lg outline-0 px-1 py-1 text-zinc-900">
+	<p bind:this={warningTextRef} class="opacity-0 transition-opacity duration-500 bg-orange-400 px-3 rounded-md w-full text-center text-zinc-900">Enter a title!</p>
 	<div class="flex justify-between w-full">
-		<button type="submit" class="bg-emerald-300 py-1 px-4">Create</button>
-		<button on:click={cancelCreateBoard} class="bg-red-600 py-1 px-4">Cancel</button>
+		<button type="submit" class="bg-emerald-300 py-1 px-4 text-zinc-900 rounded-md">Create</button>
+		<button on:click={cancelCreateBoard} class="bg-red-600 py-1 px-4 text-zinc-900 rounded-md">Cancel</button>
 	</div>
 </form>
-	{/if}
+	<span class="absolute w-[100vw] h-[100svh] top-0 left-0 z-40" on:click={cancelCreateBoard} on:keydown={cancelCreateBoard} role="button"	tabindex="0"></span>
+{/if}
