dc92b2e7a8a270ec6aebe75f359c30999d016086
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-26 22:47:47 +0200

    Query database

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,11 +1,38 @@
+import { findUser } from '$lib/db/user.js';
+import { getBoards } from '$lib/db/board.js';
+
 export async function load({ cookies }) {
 	const isLoggedIn = cookies.get('isLoggedIn') || false
 	const userID = cookies.get('userID') || null
 	const isAdmin = cookies.get('isAdmin') || false
 
-	return {
-		userID,
-		isLoggedIn,
-		isAdmin
+	if (userID) {
+		const userResponse = await findUser(userID);
+
+		if (!userResponse.success) {
+			return {
+				user: null,
+				boards: [],
+				userID,
+				isLoggedIn,
+				isAdmin
+			}
+		}
+
+		const boardsResponse = await getBoards(userResponse.userRowId)
+
+		if (!boardsResponse) {
+			return {
+				user: userResponse.success,
+				boards: []
+			}
+		}
+
+		return {
+			user: userResponse.success,
+			boards: boardsResponse.boards
+		}
+
 	}
 }
+
