7c805ffe7257976866505120fa509f8e6a631497
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-05 23:55:35 +0100

    Define Verse component

---
+++ src/lib/ui/Verse.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { id, isAddable, isMutable, content, chapterNo, verseNo } = $props()
+</script>
+
+<div class="flex relative">
+	<h5>{chapterNo}:{verseNo}</h5>
+	<p>{content}</p>
+	{#if isAddable}
+		<button class="absolute top-0 left-0 w-full h-full">
+			+
+		</button>
+	{/if}
+</div>
