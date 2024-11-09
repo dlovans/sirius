df453b00c15e959abf9f4f58fcabb32d2b4275d7
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-09 18:41:32 +0100

    Declare Book component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import Navbar from '$lib/ui/Navbar.svelte';
+	import Book from '$lib/ui/Book/Book.svelte';
 
 	let { data = [] } = $props()
 </script>
 
 <Navbar boards={ data.boards } />
+<div class="relative h-full w-full top-16 z-0">
+	<Book />
+</div>
