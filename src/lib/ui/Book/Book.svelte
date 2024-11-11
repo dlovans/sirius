e81109a651666b2e13fa95acea58127fae822ed4
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-11 22:25:58 +0100

    Style verses

--- src/lib/ui/Book/Book.svelte
+++ src/lib/ui/Book/Book.svelte
@@ -6,8 +6,8 @@
 	}
 </script>
 
-<div class="p-2 flex flex-col gap-4">
-	<div class="flex flex-row gap-2 items-center bg-emerald-400 p-1 rounded-l">
+<div class="p-2 flex flex-col gap-4 w-full overflow-hidden">
+	<div class="flex flex-row gap-2 items-center bg-emerald-400 px-3 py-2 rounded-lg justify-between overflow-hidden">
 		<label for="language">Select Language:</label>
 		<select name="langCode" id="language" class="rounded-lg text-zinc-900 p-2 outline-0 flex">
 			<option value="ar">Arabic</option>
@@ -16,16 +16,11 @@
 	</div>
 	<input type="search" placeholder="Search verses..." class="w-full h-12 rounded-lg p-4 text-xl text-zinc-900 outline-0" oninput={filterVerses} />
 
-	<div>
-		{#each verses as verse}
-			<div class="flex relative">
-				<h5>{chapterNo}:{verseNo}</h5>
-				<p>{content}</p>
-				{#if isAddable}
-					<button class="absolute top-0 left-0 w-full h-full">
-						+
-					</button>
-				{/if}
+	<div class="flex flex-col gap-3">
+		{#each verses.data as verse}
+			<div class="flex relative w-full p-3 bg-stone-900 gap-2 rounded-lg overflow-hidden">
+				<h5 class="text-2xl bg-sky-900 flex h-min py-0.5 px-1 rounded-lg">{verse?.chapterNo}:{verse.verseNo}</h5>
+				<p class="text-lg">{verse.content}</p>
 			</div>
 		{/each}
 	</div>
