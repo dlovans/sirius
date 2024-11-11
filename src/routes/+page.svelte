a05e2358856bc7b0aedfdd28b5a84491d2f4392b
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-11 22:25:36 +0100

    Add spacer block for navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 </script>
 
 <Navbar boards={ data.boards } />
-<div class="relative h-full w-full top-16 z-0">
-	<Book />
+<div class="relative h-full w-auto top-16 z-0 flex flex-row mx-5">
+	<div class="hidden w-[300px] h-full xl:flex"></div>
+	<Book verses={data} />
 </div>
