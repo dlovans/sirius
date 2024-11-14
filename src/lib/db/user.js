1249e09ba5a7ed53a754ac45c0c4a8dab003e202
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-14 21:51:41 +0100

    Define function authenticating user

--- src/lib/db/user.js
+++ src/lib/db/user.js
@@ -1,3 +1,4 @@
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 /**
  * Checks if user exists.
@@ -6,4 +7,29 @@
  */
 export async function findUser(userId) {
 
+}
+
+/**
+ * Authenticates user.
+ * @param email {String} - Email of user.
+ * @param password {String} - Password of user.
+ * @returns {object} - Status and data for cookies.
+ */
+export async function signInUser(email, password){
+	const auth = getAuth();
+	try {
+		const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+
+		// TODO: Call findUser to get if user is admin.
+		return {
+			status: 200,
+			userID: userCredentials.user.uid,
+			isAdmin: true
+		}
+	} catch(error) {
+		return {
+			status: 422,
+			message: "Invalid email or password."
+		}
+	}
 }
