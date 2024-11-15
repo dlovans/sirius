76c0eb662c514901e4094f0ecd4896ba1fed4a9b
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-15 09:59:03 +0100

    Update layout load function

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,11 @@
-import { findUser } from '$lib/db/user.js';
-import { getBoards } from '$lib/db/topic.js';
-
 export async function load({ cookies }) {
 	const userID = cookies.get('userID') || null
 	const isAdmin = cookies.get('isAdmin') || false
+
+	return {
+		userID,
+		isAdmin,
+		topics: []
+	}
 }
 
