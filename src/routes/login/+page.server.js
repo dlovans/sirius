aef6a7d43867c31e0ace63385027ca7a953b6733
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-15 09:59:37 +0100

    Update signin form action

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,4 +1,5 @@
 import { signInUser } from '$lib/db/user.js';
+import { error, fail, redirect } from '@sveltejs/kit';
 
 export const actions = {
 	signin: async ({ cookies, request }) => {
@@ -6,5 +7,15 @@ export const actions = {
 		// TODO: Serverside basic validation, later.
 
 		const response = await signInUser(data.get('email'), data.get('password'))
+
+		if (response.status === 200) {
+			cookies.set('userID', response.userID, { path: '/' })
+			cookies.set('isAdmin', response.isAdmin, { path: '/' })
+			redirect(303,'/')
+		} else if (response.status === 422) {
+			return fail(400, { message: response.message })
+		} else {
+			throw error(500, 'Something went wrong!')
+		}
 	}
 }
