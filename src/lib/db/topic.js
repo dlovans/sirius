e64e36bfe987ee96de65098d6ea1de61252f1b1d
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-15 13:15:00 +0100

    Define function that create a topic in database

--- src/lib/db/topic.js
+++ src/lib/db/topic.js
@@ -1,6 +1,43 @@
+import { findUser } from '$lib/db/user.js';
+import { collection, addDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '$lib/db/firebase.js';
 
-export async function createTopic(userId, boardTitle) {
 
+/**
+ * Creates a topic.
+ * @param userId - ID of user creating topic.
+ * @param topicTitle - Title of topic.
+ * @returns {object} - Topic ID and status.
+ */
+export async function createTopic(userId, topicTitle) {
+	try {
+		const user = await findUser(userId)
+
+		if (user.status !== 200) {
+			return {
+				status: 401,
+				message: "Unauthorized user."
+			}
+		}
+
+		const topicRef = await addDoc(collection(db, 'topics'), {
+			topicOwner: doc(db, 'users', userId),
+			topicTitle: topicTitle,
+			verses: [],
+			content: "",
+			lastUpdated: serverTimestamp()
+		})
+
+		return {
+			status: 200,
+			topicID: topicRef.id
+		}
+	} catch(error) {
+		return {
+			status: 500,
+			message: "Something went wrong."
+		}
+	}
 }
 
 export async function getTopic(userId) {
