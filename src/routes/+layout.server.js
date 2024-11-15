be2577778d3aafa9c2e405c438866ee90e0bf28d
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-15 13:28:14 +0100

    Update layout load function to get all user topics if signed in

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,6 +2,20 @@ export async function load({ cookies }) {
 	const userID = cookies.get('userID') || null
 	const isAdmin = cookies.get('isAdmin') || false
 
+	if (userID) {
+		const topics = await getUserTopics(userID)
+
+		if (topics.status !== 200) {
+			throw error(topics.status, topics.message)
+		}
+
+		return {
+			userID,
+			isAdmin,
+			topics: topics.data
+		}
+	}
+
 	return {
 		userID,
 		isAdmin,
