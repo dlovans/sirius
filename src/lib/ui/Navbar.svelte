45c18240d8d3628620d8e5e045c2344e5d69a637
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-21 00:34:43 +0100

    Update navbar style

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -4,7 +4,6 @@
 
 	let { userID, isAdmin, topics = [] } = $props();
 
-
 	let menuRef = $state();
 	let lineOneRef = $state();
 	let lineTwoRef = $state();
@@ -53,28 +52,28 @@
 </div>
 <span bind:this={invisibleOverlayRef} onclick={toggleMenu} onkeydown={toggleMenu} class="bg-transparent w-[100vw] h-[100svh] fixed top-0 left-0 hidden z-20" tabindex="0" role="button"></span>
 <nav bind:this={menuRef} class="w-[250px] h-[40px] fixed top-0 right-0 translate-x-full xl:left-0 xl:translate-x-0 transition-all duration-300 z-20">
-	<div class="text-xl w-full h-[100svh] bg-stone-900 py-3 px-3 overflow-x-hidden">
+	<div class="text-xl w-full h-[100svh] bg-stone-900 py-3 px-3 overflow-x-hidden no-scrollbar">
 		<div class="mt-14 flex flex-col justify-center items-center gap-2">
 			{#if !userID}
-			<a href="/login" class="w-full h-10 flex items-center justify-center bg-blue-500 rounded-md">Log In</a>
-			<a href="/signup" class="w-full h-10 flex items-center justify-center bg-emerald-500 rounded-md">Sign Up</a>
+			<a href="/login" class="w-full h-10 flex items-center justify-center border border-solid border-emerald-400 rounded-md hover:bg-emerald-400 duration-500">Log In</a>
+			<a href="/signup" class="w-full h-10 flex items-center justify-center rounded-md bg-blue-500 duration-500">Sign Up</a>
 			{/if}
 			{#if userID}
-				<a href="/logout" class="w-full h-10 flex items-center justify-center bg-amber-800 rounded-md">Log Out</a>
+				<a href="/logout" class="w-full h-10 flex items-center justify-center border border-solid border-red-400 rounded-md hover:bg-red-400 duration-500">Log Out</a>
 			{/if}
 			{#if isAdmin}
-				<a href="/admin" class="w-full h-10 flex items-center justify-center bg-sky-900 bg-opacity-50 rounded-md">Administrative</a>
+				<a href="/admin" class="w-full h-10 flex items-center justify-center  bg-opacity-50 rounded-md border border-solid border-sky-900 hover:bg-sky-900">Administrative</a>
 			{/if}
 		</div>
 		<hr class="w-52 h-0.5 mx-auto my-4 border-0 rounded md:my-5 bg-gray-700">
-		<a href="/" class="w-full h-10 flex items-center justify-center bg-emerald-500 rounded-md">Qur'an</a>
+		<a href="/" class="w-full h-10 flex items-center justify-center rounded-md border border-solid border-emerald-400 hover:bg-emerald-400 duration-500">Qur'an</a>
 		<hr class="w-52 h-0.5 mx-auto my-4 border-0 rounded md:my-5 bg-gray-700">
 		<div class="flex flex-col justify-center items-center gap-2">
 			<h4>TOPICS</h4>
-			<button onclick={checkUserCredentials} class="rounded-md w-full h-10 bg-orange-500">+ Create Topic</button>
+			<button onclick={checkUserCredentials} class="rounded-md w-full h-10 border border-solid border-emerald-400 hover:bg-emerald-400 duration-500">+ Create Topic</button>
 			{#if topics.length > 0}
 				{#each topics as topic}
-					<a href="/{ topic.id }" class="w-full h-10 flex items-center justify-center bg-gray-700 bg-opacity-5 rounded-md">{ topic.title }</a>
+					<a href="/{ topic.id }" class="w-full h-10 flex items-center justify-center bg-opacity-5 rounded-md border-emerald-400 border-solid border hover:bg-emerald-400 duration-500">{ topic.topicTitle }</a>
 				{/each}
 			{/if}
 		</div>
