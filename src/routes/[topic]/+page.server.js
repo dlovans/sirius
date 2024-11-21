7eca3679ca386849d307c3fbc345c3525e2190ee
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-21 12:15:56 +0100

    Retrieve data from database

--- src/routes/[topic]/+page.server.js
+++ src/routes/[topic]/+page.server.js
@@ -1,9 +1,16 @@
 import { error } from '@sveltejs/kit';
 import { getTopic } from '$lib/db/topic.js';
+import { getVersesByLangCode } from '$lib/db/verse.js';
 
 export async function load({ params, cookies }) {
 	const userID = cookies.get('userID')
 	const topicID = params.topic
+	let langCode = cookies.get('langCode')
+	if (langCode === 'undefined' || langCode === undefined) {
+		langCode = 'en';
+	}
+
+	const verses = await getVersesByLangCode(langCode)
 
 	if (!userID) {
 		throw error(401, 'Unauthorized.')
@@ -18,7 +25,9 @@ export async function load({ params, cookies }) {
 		topicID: userTopic.id,
 		topicTitle: userTopic.topicTitle,
 		topicContent: userTopic.content,
-		// topicVerses: userTopic.verses
+		// topicVerses: userTopic.verses,
+		verses: verses,
+		langCode
 	}
 
 }
