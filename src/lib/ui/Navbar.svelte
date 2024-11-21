29298229a9749334e45c429893f1cd0ec381b5d5
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-21 12:17:17 +0100

    Update handling logout in Navbar.svelte

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -39,7 +39,11 @@
 		displayModal = false;
 	}
 
-	// TODO: Remove sample topic after populating db
+	function handleLogout() {
+		window.location.href = '/logout'
+	}
+
+
 </script>
 
 <div class="px-2 py-2 fixed flex items-center w-full z-50">
@@ -59,7 +63,7 @@
 			<a href="/signup" class="w-full h-10 flex items-center justify-center rounded-md bg-blue-500 duration-500">Sign Up</a>
 			{/if}
 			{#if userID}
-				<a href="/logout" class="w-full h-10 flex items-center justify-center border border-solid border-red-400 rounded-md hover:bg-red-400 duration-500">Log Out</a>
+				<button onclick={handleLogout} class="w-full h-10 flex items-center justify-center border border-solid border-red-400 rounded-md hover:bg-red-400 duration-500">Log Out</button>
 			{/if}
 			{#if isAdmin}
 				<a href="/admin" class="w-full h-10 flex items-center justify-center  bg-opacity-50 rounded-md border border-solid border-sky-900 hover:bg-sky-900">Administrative</a>
