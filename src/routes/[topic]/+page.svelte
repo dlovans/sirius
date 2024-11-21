aad7fee60bc7657bf4756098128daa7c0de088a5
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-21 12:16:22 +0100

    Add UI for topic routes

--- src/routes/[topic]/+page.svelte
+++ src/routes/[topic]/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Hello</h1>
+<script>
+import Book from '$lib/ui/Book/Book.svelte';
+import NavbarSpacer from '$lib/ui/NavbarSpacer.svelte';
+import Navbar from '$lib/ui/Navbar.svelte';
+
+let { data = [] } = $props()
+</script>
+
+<Navbar userID={data.userID} isAdmin={data.isAdmin} topics={data.topics} />
+<div class="relative h-full w-auto top-16 z-0 flex flex-row mx-5">
+	<NavbarSpacer />
+	<Book verses={data} />
+</div>
