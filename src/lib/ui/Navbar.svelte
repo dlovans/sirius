35d847239e3a96b34da9b3b5bb999f62e43dbe51
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-26 17:31:02 +0100

    Fix menu on small screens not closing on user interaction

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -40,7 +40,7 @@
 	}
 </script>
 
-<div class="px-2 py-2 fixed flex items-center w-full z-50">
+<div class="px-2 py-2 fixed flex items-center w-full z-50 bg-[#212529] xl:bg-transparent">
 	<a href="/" class="font-bold text-xl text-emerald-400 mr-auto">SIRIUS</a>
 	<div onclick={toggleMenu} onkeydown={toggleMenu} class="relative w-[40px] h-[40px] overflow-hidden hover:cursor-pointer xl:hidden" tabindex="0" role="button">
 		<span bind:this={lineOneRef} class="h-1 w-10 bg-emerald-400 absolute rounded-sm top-[5px] transition-all duration-300"></span>
@@ -48,30 +48,30 @@
 		<span bind:this={lineThreeRef} class="h-1 w-10 bg-emerald-400 absolute rounded-sm bottom-[5px] transition-all duration-300"></span>
 	</div>
 </div>
-<span bind:this={invisibleOverlayRef} onclick={toggleMenu} onkeydown={toggleMenu} class="bg-transparent w-[100vw] h-[100svh] fixed top-0 left-0 hidden z-20" tabindex="0" role="button"></span>
+<span bind:this={invisibleOverlayRef} onclick={toggleMenu} onkeydown={toggleMenu} tabindex="0" role="button" class="bg-transparent w-[100vw] h-[100svh] fixed top-0 left-0 hidden z-20"></span>
 <nav bind:this={menuRef} class="w-[250px] h-[40px] fixed top-0 right-0 translate-x-full xl:left-0 xl:translate-x-0 transition-all duration-300 z-20">
 	<div class="text-xl w-full h-[100svh] bg-stone-900 py-3 px-3 overflow-x-hidden no-scrollbar">
 		<div class="mt-14 flex flex-col justify-center items-center gap-2">
 			{#if !isAuthorized}
-			<a href="/login" class="w-full h-10 flex items-center justify-center border border-solid border-emerald-400 rounded-md hover:bg-emerald-400 duration-500">Log In</a>
-			<a href="/signup" class="w-full h-10 flex items-center justify-center rounded-md bg-blue-500 duration-500">Sign Up</a>
+			<a href="/login" onclick={toggleMenu} onkeydown={toggleMenu} tabindex="0" role="button" class="w-full h-10 flex items-center justify-center border border-solid border-emerald-400 rounded-md hover:bg-emerald-400 duration-500">Log In</a>
+			<a href="/signup" onclick={toggleMenu} onkeydown={toggleMenu} tabindex="0" role="button" class="w-full h-10 flex items-center justify-center rounded-md bg-blue-500 duration-500">Sign Up</a>
 			{/if}
 			{#if isAuthorized}
-				<a href="/logout" class="w-full h-10 flex items-center justify-center border border-solid border-red-400 rounded-md hover:bg-red-400 duration-500">Log Out</a>
+				<a href="/logout" onclick={toggleMenu} onkeydown={toggleMenu} tabindex="0" role="button" class="w-full h-10 flex items-center justify-center border border-solid border-red-400 rounded-md hover:bg-red-400 duration-500">Log Out</a>
 			{/if}
 			{#if isAdmin}
-				<a href="/admin" class="w-full h-10 flex items-center justify-center  bg-opacity-50 rounded-md border border-solid border-sky-900 hover:bg-sky-900">Administrative</a>
+				<a href="/admin" onclick={toggleMenu} onkeydown={toggleMenu} tabindex="0" role="button" class="w-full h-10 flex items-center justify-center  bg-opacity-50 rounded-md border border-solid border-sky-900 hover:bg-sky-900">Administrative</a>
 			{/if}
 		</div>
 		<hr class="w-52 h-0.5 mx-auto my-4 border-0 rounded md:my-5 bg-gray-700">
-		<a href="/" class="w-full h-10 flex items-center justify-center rounded-md border border-solid border-emerald-400 hover:bg-emerald-400 duration-500">Qur'an</a>
+		<a href="/" onclick={toggleMenu} onkeydown={toggleMenu} tabindex="0" role="button" class="w-full h-10 flex items-center justify-center rounded-md border border-solid border-emerald-400 hover:bg-emerald-400 duration-500">Qur'an</a>
 		<hr class="w-52 h-0.5 mx-auto my-4 border-0 rounded md:my-5 bg-gray-700">
 		<div class="flex flex-col justify-center items-center gap-2">
 			<h4>TOPICS</h4>
-			<button onclick={checkUserCredentials} class="rounded-md w-full h-10 border border-solid border-emerald-400 hover:bg-emerald-400 duration-500">+ Create Topic</button>
+			<button onclick={() => { toggleMenu(); checkUserCredentials(); }} class="rounded-md w-full h-10 border border-solid border-emerald-400 hover:bg-emerald-400 duration-500">+ Create Topic</button>
 			{#if topics.length > 0}
 				{#each topics as topic}
-					<a href="/{ topic.id }" class="w-full h-10 flex items-center justify-center bg-opacity-5 rounded-md border-emerald-400 border-solid border hover:bg-emerald-400 duration-500">{ topic.topicTitle }</a>
+					<a href="/{ topic.id }" onclick={toggleMenu} onkeydown={toggleMenu} tabindex="0" role="button" class="w-full h-10 flex items-center justify-center bg-opacity-5 rounded-md border-emerald-400 border-solid border hover:bg-emerald-400 duration-500 text-nowrap overflow-hidden px-2">{ topic.topicTitle }</a>
 				{/each}
 			{/if}
 		</div>
