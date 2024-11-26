9ce01e376ab72992facbd8c1135f1432979f7522
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-26 18:04:45 +0100

    Refactor Verse component

--- src/lib/ui/Book/Verse.svelte
+++ src/lib/ui/Book/Verse.svelte
@@ -1,8 +1,11 @@
 <script>
 	let { verseData, isAdmin, isEditable = false, isAddable = false} = $props()
 
+	// TODO: Add isRemovable prop.
+
 	let verseRef = $state()
 	let warningRef = $state()
+	let inputVerseRef = $state()
 	let timeoutID
 
 	const originalVerseContent = verseData.content
@@ -17,6 +20,7 @@
 		clearTimeout(timeoutID)
 		event.preventDefault()
 		if (verseRef.textContent !== originalVerseContent) {
+			inputVerseRef.value = verseRef.textContent
 			event.target.submit()
 		} else {
 			warningRef.classList.remove('translate-x-[250%]')
@@ -33,22 +37,26 @@
 
 </script>
 
-<form onsubmit={validateContent} action={(isEditable && verseData.isMutable && isAdmin) ? "?/updateVerse" : ""} class="flex items-center relative w-full p-3 bg-stone-900 gap-2 rounded-lg overflow-hidden shadow-inner border border-solid border-zinc-50 border-opacity-30">
-	<h5 class="text-md flex h-min py-0.5 px-1 rounded-lg">{verseData.chapterNo}:{verseData.verseNo}</h5>
-	{#if isEditable && verseData.isMutable && isAdmin}
-		<p bind:this={warningRef} class="absolute top-1/2 left-1/2 translate-x-[250%] -translate-y-1/2 opacity-0 bg-red-400 px-4 py-2 text-center rounded-md transition-all duration-500">No changes were made!</p>
-		<div class="flex gap-2 flex-col w-full">
-			<input type="hidden" name="verseId" value={verseData.verseId} />
-			<p bind:this={verseRef} contenteditable class="text-lg flex items-center border border-solid border-zinc-400 border-opacity-70 w-full p-2 rounded-md outline-none">{verseData.content}</p>
-			<div class="flex gap-2">
-				<button type="submit" class="p-2 rounded-md border border-solid border-emerald-400 hover:bg-emerald-400 hover:duration-500">Update</button>
-				<button onclick={revertChanges} type="reset" class="p-2 rounded-md border border-solid border-red-400 hover:bg-red-400 hover:duration-500">Cancel</button>
+<form onsubmit={validateContent} action={(isEditable && verseData.isMutable && isAdmin) ? "?/updateVerse" : ""} class="flex flex-col items-center relative w-full bg-stone-900 gap-2 rounded-lg overflow-hidden shadow-inner border border-solid border-zinc-50 border-opacity-30 p-3">
+	<div class="flex w-full items-center gap-1">
+		<h5 class="text-md flex h-min py-0.5 px-1 rounded-lg items-center">{verseData.chapterNo}:{verseData.verseNo}</h5>
+		{#if isEditable && verseData.isMutable && isAdmin}
+			<p bind:this={warningRef} class="absolute top-1/2 left-1/2 translate-x-[250%] -translate-y-1/2 opacity-0 bg-red-400 px-4 py-2 text-center rounded-md transition-all duration-500">No changes were made!</p>
+			<div class="flex gap-2 flex-col w-full">
+				<input type="hidden" name="verseId" value={verseData.verseId} />
+				<input type="hidden" name="verseContent" value="" bind:this={inputVerseRef}>
+				<p bind:this={verseRef} contenteditable class="text-lg flex items-center border border-solid border-zinc-400 border-opacity-70 w-full p-2 rounded-md outline-none">{verseData.content}</p>
+				<div class="flex gap-2">
+					<button type="submit" class="p-2 rounded-md border border-solid border-emerald-400 hover:bg-emerald-400 hover:duration-500">Update</button>
+					<button onclick={revertChanges} type="reset" class="p-2 rounded-md border border-solid border-red-400 hover:bg-red-400 hover:duration-500">Cancel</button>
+				</div>
 			</div>
-		</div>
-	{:else}
-		<p class="text-lg flex items-center">{verseData.content}</p>
-	{/if}
+		{:else}
+			<p class="text-lg flex items-center">{verseData.content}</p>
+		{/if}
+	</div>
 	{#if isAddable}
-		<button class=" bg-emerald-400 p-2 ml-auto">+</button>
+		<button class="bg-emerald-400 p-2 w-full">+ Add Verse</button>
 	{/if}
+
 </form>
