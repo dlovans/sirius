562a4918a5348b85663683825d4fd21a8706243f
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-26 21:12:32 +0100

    Define function that updated a single verse

--- src/lib/db/verse.js
+++ src/lib/db/verse.js
@@ -1,5 +1,6 @@
 import { db } from '$lib/db/firebase.js'
-import { collection, query, where, getDocs } from 'firebase/firestore'
+import { collection, query, where, getDocs, doc, getDoc, setDoc } from 'firebase/firestore'
+import { isAuthenticated } from '$lib/db/user.js';
 
 /**
  * Gets all verse documents from database.
@@ -19,13 +20,77 @@ export async function getVersesByLangCode(languageCode) {
 			}
 		}
 
-		const verses = querySnapshot.docs.map(doc => doc.data())
+		const verses = querySnapshot.docs.map(doc => ({
+			id: doc.id,
+			...doc.data()
+		}));
 
 		return {
 			status: 200,
 			data: verses
 		}
 	} catch (error) {
+		return {
+			status: 500,
+			message: error.message
+		}
+	}
+}
+
+/**
+ * Updates a verse.
+ * @param verseId - Verse ID in database.
+ * @param updatedContent - Updated verse content.
+ * @returns - If successful.
+ */
+export async function updateVerseById(verseId, updatedContent) {
+	try {
+		const isAuthorized = await isAuthenticated()
+
+		if (isAuthorized.status !== 200) {
+			return {
+				status: 404,
+				message: "Could not update verse."
+			}
+		}
+
+		if (!isAuthorized.isAuthenticated || !isAuthorized.isAdmin) {
+			return {
+				status: 401,
+				message: "Unauthorized user. Verse was not updated."
+			}
+		}
+
+		const docRef = doc(db, 'verses', verseId)
+		const docSnap = await getDoc(docRef)
+
+		if (!docSnap.exists()) {
+			return {
+				status: 422,
+				message: "Could not find verse to be updated."
+			}
+		}
+
+		const data = docSnap.data()
+		const { isMutable } = data
+
+		if (!isMutable) {
+			return {
+				status: 403,
+				message: "Cannot change specific verse."
+			}
+		}
+
+		await setDoc(docRef, { content: updatedContent }, { merge: true })
+
+		return {
+			status: 200,
+			message: "Verse successfully updated."
+		}
+
+
+	} catch (error) {
+		console.error(error.message)
 		return {
 			status: 500,
 			message: error.message
