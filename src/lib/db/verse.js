6279049398b63e50a7344a49182321f4bf5dc653
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-26 22:13:05 +0100

    Define function adding verse to topic verses collection. Refactor setDoc to updateDoc

--- src/lib/db/verse.js
+++ src/lib/db/verse.js
@@ -1,6 +1,7 @@
 import { db } from '$lib/db/firebase.js'
-import { collection, query, where, getDocs, doc, getDoc, setDoc } from 'firebase/firestore'
+import { collection, query, where, getDocs, doc, getDoc, updateDoc, arrayUnion } from 'firebase/firestore'
 import { isAuthenticated } from '$lib/db/user.js';
+import { getTopic } from '$lib/db/topic.js';
 
 /**
  * Gets all verse documents from database.
@@ -81,11 +82,57 @@ export async function updateVerseById(verseId, updatedContent) {
 			}
 		}
 
-		await setDoc(docRef, { content: updatedContent }, { merge: true })
+		await updateDoc(docRef, { content: updatedContent })
 
 		return {
 			status: 200,
 			message: "Verse successfully updated."
+		}
+
+
+	} catch (error) {
+		console.error(error.message)
+		return {
+			status: 500,
+			message: error.message
+		}
+	}
+}
+
+/**
+ * Adds verse to the topic verse collection.
+ * @param verseId - ID of verse to be added to topic verse collection.
+ * @param topicId - ID of topic to be updated.
+ * @returns - If successful.
+ */
+export async function addVerseToTopic(verseId, topicId) {
+	try {
+		const topic = await getTopic(topicId)
+		if (topic.status !==200) {
+			return {
+				status: topic.status,
+				message: topic.message
+			}
+		}
+
+		if (Array.isArray(topic.verses) && topic.verses.length !== 0) {
+			for (const verse of topic.verses) {
+				if (verseId === verse) {
+					return {
+						status: 409,
+						message: "Verse already in collection."
+					};
+				}
+			}
+		}
+
+
+		const docRef = doc(db, 'topics', topicId)
+		await updateDoc(docRef, { verses: arrayUnion(verseId) })
+
+		return {
+			status: 200,
+			message: `Verse successfully added to topic with ID: ${topicId}.`
 		}
 
 
