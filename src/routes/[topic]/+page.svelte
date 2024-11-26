2d9a75c07c99965f5692e6d0c579c151e3025933
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-26 18:04:26 +0100

    Update topic route responsiveness

--- src/routes/[topic]/+page.svelte
+++ src/routes/[topic]/+page.svelte
@@ -8,23 +8,20 @@ console.log(data)
 </script>
 
 <Navbar isAuthorized={data.isAuthorized} isAdmin={data.isAdmin} topics={data.topics} />
-<div class="relative h-full w-auto top-16 z-0 flex flex-row mx-1 overflow-hidden">
+<div class="relative h-full w-auto top-16 z-0 flex flex-row overflow-hidden">
 	<NavbarSpacer />
 	<div class="p-1 xl:px-7 flex flex-col gap-2 w-full overflow-hidden">
 		<h2 class="w-full text-xl py-2 px-4 bg-emerald-400 rounded-md">Topic: {data.topicTitle}</h2>
 		<div class="flex flex-row gap-2 overflow-x-auto w-full h-[83svh] snap-mandatory snap-x">
-			<div class="w-[93vw] h-full flex-shrink-0 xl:flex-shrink snap-center relative">
+			<div class="w-[98vw] h-full flex-shrink-0 lg:flex-shrink snap-center relative">
 				<Book data={data} isAddable />
 			</div>
-			<div class="w-[93vw] h-full flex-shrink-0 xl:flex-shrink snap-center">
+			<div class="w-[98vw] h-full flex-shrink-0 lg:flex-shrink snap-center">
 				<Book data={data} isAddable />
 			</div>
-			<div class="w-[93vw] h-full flex-shrink-0 xl:flex-shrink snap-center">
+			<div class="w-[98vw] h-full flex-shrink-0 lg:flex-shrink snap-center">
 				<Book data={data} isAddable />
 			</div>
 		</div>
-
-
-
 	</div>
 </div>
