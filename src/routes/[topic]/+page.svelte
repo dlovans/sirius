ffa4fc2d6835bfb39dae4c9dab5446bedd9e0fed
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-26 19:52:22 +0100

    Update props passed to Book

--- src/routes/[topic]/+page.svelte
+++ src/routes/[topic]/+page.svelte
@@ -13,14 +13,13 @@ console.log(data)
 	<div class="p-1 xl:px-7 flex flex-col gap-2 w-full overflow-hidden">
 		<h2 class="w-full text-xl py-2 px-4 bg-emerald-400 rounded-md">Topic: {data.topicTitle}</h2>
 		<div class="flex flex-row gap-2 overflow-x-auto w-full h-[83svh] snap-mandatory snap-x">
-			<div class="w-[98vw] h-full flex-shrink-0 lg:flex-shrink snap-center relative">
-				<Book data={data} isAddable />
-			</div>
 			<div class="w-[98vw] h-full flex-shrink-0 lg:flex-shrink snap-center">
 				<Book data={data} isAddable />
 			</div>
 			<div class="w-[98vw] h-full flex-shrink-0 lg:flex-shrink snap-center">
-				<Book data={data} isAddable />
+				<Book data={data} isRemovable />
+			</div>
+			<div class="w-[98vw] h-full flex-shrink-0 lg:flex-shrink snap-center">
 			</div>
 		</div>
 	</div>
