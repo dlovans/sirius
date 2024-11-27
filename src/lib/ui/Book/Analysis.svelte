809c75078072bb47e2ecdd4b0409e45bb3ad4ee0
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-27 15:45:21 +0100

    Create Analysis component

---
+++ src/lib/ui/Book/Analysis.svelte
@@ -0,0 +1,17 @@
+<script>
+	let { analysisContent } = $props()
+	let textareaRef= $state()
+
+	function validateContent(event) {
+		event.preventDefault()
+		if (textareaRef.value !== analysisContent) {
+			event.target.submit()
+		}
+	}
+</script>
+
+<form action="?/updateAnalysis" onsubmit={validateContent} class=" h-full w-full flex flex-col gap-1">
+	<label for="analysis" class="hidden">Analysis</label>
+	<textarea bind:this={textareaRef} wrap="soft" name="analysis" id="analysis" class="whitespace-pre-wrap h-[calc(100%-5svh)] overflow-x-hidden bg-stone-900 resize-none outline-none border border-solid border-emerald-400 rounded-md px-2 py-1">{analysisContent}</textarea>
+	<button type="submit" class="h-">Save</button>
+</form>
