e40c3bd10bdd9334317abf0f5711659e871f9ed9
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-27 16:25:39 +0100

    Update Analysis.svelte UI

--- src/lib/ui/Book/Analysis.svelte
+++ src/lib/ui/Book/Analysis.svelte
@@ -12,6 +12,6 @@
 
 <form action="?/updateAnalysis" onsubmit={validateContent} class=" h-full w-full flex flex-col gap-1">
 	<label for="analysis" class="hidden">Analysis</label>
-	<textarea bind:this={textareaRef} wrap="soft" name="analysis" id="analysis" class="whitespace-pre-wrap h-[calc(100%-5svh)] overflow-x-hidden bg-stone-900 resize-none outline-none border border-solid border-emerald-400 rounded-md px-2 py-1">{analysisContent}</textarea>
-	<button type="submit" class="h-">Save</button>
+	<textarea bind:this={textareaRef} wrap="soft" name="analysis" id="analysis" class="whitespace-pre-wrap h-full overflow-x-hidden bg-stone-900 resize-none outline-none border border-solid border-emerald-400 rounded-md px-2 py-1">{analysisContent}</textarea>
+	<button type="submit" class="w-full border border-solid border-emerald-400 py-2 rounded-md hover:bg-emerald-400 duration-300">Save</button>
 </form>
