8a500f92c52745b982043b12b9078522d52ea56c
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-27 16:23:21 +0100

    Fix layout issue at topic routes

--- src/routes/[topic]/+page.svelte
+++ src/routes/[topic]/+page.svelte
@@ -8,7 +8,7 @@ let { data } = $props()
 </script>
 
 <Navbar isAuthorized={data.isAuthorized} isAdmin={data.isAdmin} topics={data.topics} />
-<div class="relative h-full w-auto top-16 xl:top-5 z-0 flex flex-row overflow-hidden">
+<div class="relative w-auto top-16 xl:top-0 xl:h-[100svh] z-0 flex flex-row overflow-hidden">
 	<NavbarSpacer />
 	<div class="p-1 xl:px-4 flex flex-col gap-2 w-full overflow-hidden">
 		<div class="w-full text-xl py-2 px-4 bg-emerald-400 rounded-md flex flex-col lg:flex-row gap-1">
@@ -22,21 +22,20 @@ let { data } = $props()
 				<button type="submit" class="px-3 py-1 rounded-md bg-blue-500 w-24">Update</button>
 			</form>
 			<form action="?/deleteTopic">
-				<input type="hidden" name="topicTitle" value={data.topicTitle} />
 				<input type="hidden" name="topicId" value={data.topicId} />
 				<button type="submit" class="px-3 py-1 rounded-md bg-red-400 w-24">Delete</button>
 			</form>
 		</div>
-		<div class="flex flex-row gap-2 overflow-x-auto w-full h-[83svh] xl:h-[90svh] snap-mandatory snap-x">
-			<div class="w-[98vw] xl:w-[50vw] h-full flex-shrink-0 lg:flex-shrink snap-center flex flex-col gap-2">
+		<div class="flex flex-row gap-2 overflow-x-auto w-full h-[75svh] lg:h-[85svh] xl:h-full snap-mandatory snap-x">
+			<div class="w-[98vw] xl:w-[50vw] flex-shrink-0 lg:flex-shrink snap-center flex flex-col gap-2">
 				<h3 class="text-center w-full bg-emerald-200 text-stone-900 rounded-md">Add verses for analysis</h3>
 				<Book data={data} isAddable />
 			</div>
-			<div class="w-[98vw] xl:w-[50vw] h-full flex-shrink-0 lg:flex-shrink snap-center flex flex-col gap-2">
+			<div class="w-[98vw] xl:w-[50vw] flex-shrink-0 lg:flex-shrink snap-center flex flex-col gap-2">
 				<h3 class="text-center w-full bg-emerald-200 text-stone-900 rounded-md">Verses for analysis</h3>
 				<Book data={data} isRemovable />
 			</div>
-			<div class="w-[98vw] xl:w-[100vw] h-full flex-shrink-0 lg:flex-shrink snap-center flex flex-col gap-2">
+			<div class="w-[98vw] xl:w-[100vw] flex-shrink-0 lg:flex-shrink snap-center flex flex-col gap-2">
 				<h3 class="text-center w-full bg-emerald-200 text-stone-900 rounded-md">Analysis</h3>
 				<Analysis analysisContent={data.analysis} />
 			</div>
