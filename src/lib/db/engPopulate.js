a353777a9cd07b3b19a51c9da0deace46263369a
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-30 22:50:32 +0100

    Populate firestore with verses

---
+++ src/lib/db/engPopulate.js
@@ -0,0 +1,13 @@
+// import { db } from '$lib/db/firebase.js'
+// import { collection, addDoc } from "firebase/firestore";
+// import  engVerses from './engVerses.json' assert {type: 'json'}
+//
+// export async function populateDB() {
+// 	console.log('populateDB')
+// 	for (const verse of engVerses) {
+// 		await addDoc(collection(db, 'verses'), verse)
+// 	}
+// }
+//
+//
+//
